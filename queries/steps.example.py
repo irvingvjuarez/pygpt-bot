78d867784957a07a07f10cbdd7359fb87fc06c71
Irving Juárez <irvingjuarez274@gmail.com>
2023-05-05 18:35:25 -0500

    [UPDATE] name of previous examples [ADD] steps.example

---
+++ queries/steps.example.py
@@ -0,0 +1,47 @@
+import sys
+sys.path.append("/home/irving/pygpt-bot/")
+
+from utils.get_completion import get_completion
+
+text_1 = f"""
+Making a cup of tea is easy! First, you need to get some \
+water boiling. While that's happening, \
+grab a cup and put a tea bag in it. Once the water is \
+hot enough, just pour it over a tea bag. \
+Let it sit for a bit so the tea can steep. After a \
+few minutes, take out the tea bag. If you \
+like, you can add some sugar or milk to taste. \
+And that's it! You've got yourself a delicious \
+cup of tea. Enjoy!
+"""
+
+text_2 = f"""
+The sun is shining brightly today, and the birds are \
+singing. It's a beautiful day to go for a \
+walk in the park. The flowers are blooming, and the \
+trees are swaying gently in the breeze. People \
+are out and about, enjoying the lovely weather. \
+Some are having picnics, while others are playing \
+games or simply relaxing on the grass. It's a \
+perfect day to spend time outdoors and appreciate the \
+beauty of nature.
+"""
+
+prompt = f"""
+You will be provided wit text delimited by triple backticks.
+If it contains a sequence of instructions, \
+re-write those instructions in the following format:
+
+Step 1 - ...
+Step 2 - ...
+...
+Step N - ...
+
+Otherwise, return the same content in a shorter way.
+
+In both cases, add a title at the top (indicating it is the title).
+```{text_1}```
+"""
+
+response = get_completion(prompt)
+print(response)
